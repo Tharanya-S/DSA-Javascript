bf5cea37df6a543ee87a0b923b076a27675796d3
Tharanya <tharanyasugumaran@gamil.com>
2025-06-18 11:15:38 +0530

    WarmUp

---
+++ Programming_101.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+console.log(2 + 2);
+const a = 10;
+const b = 20;
+const sum = a + b;
+const multiply = a * b;
+const difference = b - a;
+console.log(sum);
+console.log(multiply);
+console.log(difference);
+let firstName = "Tharanya";
+let lastName = "Sugumaran";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let arr = [5, 10, 2, 0, 1];
+console.log(arr[0]);
+let stringArr = ["Thara", 26, false, "Anya"];
+console.log(stringArr[1]);
+let multipleArr = [
+  [1, 2],
+  [3, 4],
+];
+console.log(multipleArr[1][0]);
+let obj = {
+  a: 1,
+  bool: true,
+  fistName: "Thara",
+  lastName: "S",
+};
+console.log(obj.fistName + " " + obj.lastName);
